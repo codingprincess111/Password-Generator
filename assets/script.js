35653a800036886cb28266afd9579eadda7e9759
codingprincess111 <100244609+codingprincess111@users.noreply.github.com>
2023-03-20 14:23:51 -0500

    password prompts

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
 // Assignment code here
+var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numberCharacters = ["0","1","2","3","4","5","6","7","8","9"]
 
+function generatePasswordPrompts() {
+    var length = prompt("enter length between of at least 8 characters and no more than 128 characters")
+    console.log (length);
+    if(length < 8 || length > 128) {
+        alert("Password must be at least 8 characters and no more than 128 characters")
+    }
+var hasLowercase = confirm("click ok to include lowercase")
+var hasUppercase = confirm ("click ok to include uppercase")
+var hasNumbers = confirm ("click ok to include numbers")
+var passwordPrompts = {
+    length: length, 
+    hasLowercase: hasLowercase,
+    hasUppercase: hasUppercase,
+    hasNumbers: hasNumbers
+
+}
+return passwordPrompts
+}
+
+function generatePassword () {
+    var options = generatePasswordPrompts ();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
